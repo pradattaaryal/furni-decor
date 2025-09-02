8aae7ad798f2e97a3136943f6baa0cf391818e7c
pradattaaryal <pradattaaryal2468@example.com>
2025-09-02 13:40:59 +0545

    minor fixes

--- src/common/configs/app.config.ts
+++ src/common/configs/app.config.ts
@@ -9,9 +9,6 @@ export default registerAs(
         : 3000,
     },
     env: process.env.APP_ENV,
-    maxDatabaseTableBackup: process.env.MAX_DATABASE_TABLE_BACKUP_FILE,
-    maxDatabaseBackup: process.env.MAX_DATABASE_BACKUP_FILE,
     email: process.env.APP_EMAIL,
-    geo_api_key: process.env.GEO_IP_API_KEY,
   }),
 );
