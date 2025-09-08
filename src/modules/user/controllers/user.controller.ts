f5f6778bc2e5f254c7bf4b9865f605a10975ea67
pradatta <aryal.pradatta@webexpertsnepal.com>
2025-09-08 16:03:29 +0545

    create employee

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -5,6 +5,7 @@ import { IResponse } from 'src/common/response/interfaces/response.interface';
 import { UserCreateDto } from '../dto/user.create.dto';
 import { UserEntity } from '../entities/user.entity';
 import { UserService } from '../services/user.service';
+import { MarketingUserCreateDto } from '../dto/marketing.create.dto';
 
 @SerializeOptions({
   groups: ADMIN_ONLY_GROUP,
@@ -12,29 +13,45 @@ import { UserService } from '../services/user.service';
 @ApiTags('users')
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly _UserService: UserService,
-   ) {}
+  constructor(private readonly _UserService: UserService) {}
 
-    @Post('/create')
-    @ApiOperation({ summary: 'Register a new user' })
-    async register(@Body() registerDto: UserCreateDto): Promise<
-      IResponse<{
-        otp: string;
-  
-        message: string;
-      }>
-    > {
-      const result = await this._UserService.register(registerDto);
-  
-      result.user as UserEntity;
-  
-      return {
-        data: {
-          otp: result.otp,
-  
-          message: 'User registered successfully. Please verify your OTP.',
-        },
-      };
-    }
+  @Post('/create')
+  @ApiOperation({ summary: 'Register a new user' })
+  async register(@Body() registerDto: UserCreateDto): Promise<
+    IResponse<{
+      otp: string;
+
+      message: string;
+    }>
+  > {
+    const result = await this._UserService.register(registerDto);
+
+    result.user as UserEntity;
+
+    return {
+      data: {
+        otp: result.otp,
+
+        message: 'User registered successfully. Please verify your OTP.',
+      },
+    };
+  }
+
+  @Post('/create-emplyee')
+  @ApiOperation({ summary: 'Register a new employee' })
+  async registerEmployee(@Body() registerDto: MarketingUserCreateDto): Promise<
+    IResponse<{
+      message: string;
+    }>
+  > {
+    const result = await this._UserService.registerEmployee(registerDto);
+
+    result.user as UserEntity;
+
+    return {
+      data: {
+        message: 'User registered successfully. Please verify your OTP.',
+      },
+    };
+  }
 }
